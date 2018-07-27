8f89d095ce08e91f63babf18a9864db7406f4276
Ivan Frolov <ivan@frolov.md>
2018-07-27 22:35:07 +1000

    Implement column width calculation

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   idea
   scala
   maven
+  id("com.github.maiflai.scalatest") version "0.22"
 }
 
 repositories {
@@ -11,4 +12,5 @@ repositories {
 dependencies {
   compile("org.scala-lang:scala-library:${properties["scalaVersion"]}")
   testCompile("org.scalatest:scalatest_${properties["scalaMajor"]}:3.0.5")
+  testRuntime("org.pegdown:pegdown:1.4.2")
 }
---
+++ src/test/scala/io/github/asciitable/StreamingTest.scala
@@ -0,0 +1,56 @@
+package io.github.asciitable
+
+import java.io.{ByteArrayOutputStream, OutputStream}
+
+import org.scalatest.{FreeSpec, Matchers}
+
+class StreamingTest extends FreeSpec with Matchers {
+  "Don't lose or repeat sampled rows" in {
+    val baos        = new ByteArrayOutputStream()
+    val rowIterator = new RowProvider(cnt => Seq(s"column1, row$cnt", s"column2, row$cnt", s"column3, row$cnt"))
+    AsciiTable()
+      .multiline(false)
+      .sampleAtMostRows(5)
+      .rows(rowIterator.take(10))
+      .write(baos)
+
+    baos.toString shouldBe
+      """┌─────────────┬─────────────┬─────────────┐
+        |│column1, row1│column2, row1│column3, row1│
+        |│column1, row2│column2, row2│column3, row2│
+        |│column1, row3│column2, row3│column3, row3│
+        |│column1, row4│column2, row4│column3, row4│
+        |│column1, row5│column2, row5│column3, row5│
+        |│column1, row6│column2, row6│column3, row6│
+        |│column1, row7│column2, row7│column3, row7│
+        |│column1, row8│column2, row8│column3, row8│
+        |│column1, row9│column2, row9│column3, row9│
+        |│column1, row…│column2, row…│column3, row…│
+        |└─────────────┴─────────────┴─────────────┘
+        |""".stripMargin
+  }
+
+  "Test large set" ignore {
+    val devnull = new OutputStream {
+      var size                         = 0L
+      override def write(b: Int): Unit = size += 1
+    }
+    val row         = Seq.fill(10)("1234567890" * 20)
+    val rowProvider = new RowProvider(Function.const(row))
+    AsciiTable()
+      .multiline(false)
+      .sampleAtMostRows(5)
+      .rows(rowProvider.take(10000000))
+      .write(devnull)
+    devnull.size shouldBe 1230000482L
+  }
+
+  class RowProvider(row: Int => Seq[String]) extends Iterator[Seq[String]] {
+    private var cnt               = 0
+    override def hasNext: Boolean = true
+    override def next(): Seq[String] = {
+      cnt += 1
+      row(cnt)
+    }
+  }
+}
